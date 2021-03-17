140409c3c35587dbe2228af8b76f937baed524a0
Neman19 <80602926+Neman19@users.noreply.github.com>
2021-03-18 00:02:37 +0300

    Delete step-3.py

--- step-3.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 17 00:47:17 2021
-
-@author: Ya
-"""
-n = int(input("Введите число - "))
-total = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
-print("Сумма чисел n + nn + nnn - %d" % total)
