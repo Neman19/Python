4e1a4b6b6e59bab16e5c8ab7e924597f5ba33089
Neman19 <80602926+Neman19@users.noreply.github.com>
2021-03-17 23:26:05 +0300

    lesson-1

---
+++ step -1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Ya
+"""
+name = input("Введите Ваше имя: ")
+second_name = input("Введите Вашу фамилию: ")
+how_old = int(input("Введите Ваш возраст: "))
+
+print(name, second_name, how_old, "лет")
---
+++ step-4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 01:12:46 2021
+
+@author: Ya
+"""
+n = abs(int(input("Введите целое положительное число ")))
+max = n % 10
+while n >= 1:
+    n = n // 10
+    if n % 10 > max:
+        max = n % 10
+    if n > 9:
+        continue
+    else:
+        print("Максимальное цифра в числе ", max)
+        break
